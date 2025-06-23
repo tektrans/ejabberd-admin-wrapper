77e9810d3d4cfc4cd0bbd5b35111b4f4b4052848
Adhidarma Hadiwinoto <adhisimon@tektrans.id>
2025-06-24 05:28:06 +0700

    write pid to app.pid

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 /* eslint-disable import/first */
 process.cwd(import.meta.dirname);
 
+import { writeFile } from 'node:fs/promises';
 import constants from './lib/constants.mjs';
 import logger from './lib/logger.mjs';
 import sdNotify from './lib/sd-notify.mjs';
@@ -13,6 +14,7 @@ logger.info(`${MODULE_NAME} 6373B0B4: Initializing`, {
 
 await initWebUI();
 await sdNotify();
+await writeFile('app.pid', process.pid.toString());
 
 logger.info(`${MODULE_NAME} 55E28073: Initialized`, {
   localUrl: `http://localhost:${constants.webuiListenPort}`,
