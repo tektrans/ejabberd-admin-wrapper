9fac96a2c31768626f2816e4c22a3f04399d0222
Adhidarma Hadiwinoto <adhisimon@tektrans.id>
2025-06-24 01:58:09 +0700

    Version info on init log

--- index.mjs
+++ index.mjs
@@ -1,10 +1,19 @@
 /* eslint-disable import/first */
 process.cwd(import.meta.dirname);
 
+import constants from './lib/constants.mjs';
 import logger from './lib/logger.mjs';
 import initWebUI from './lib/webui/index.mjs';
 
 const MODULE_NAME = 'MAIN';
-logger.info(`${MODULE_NAME} 6373B0B4: Initializing`);
+logger.info(`${MODULE_NAME} 6373B0B4: Initializing`, {
+  version: constants.version
+});
 
 await initWebUI();
+
+logger.info(`${MODULE_NAME} 55E28073: Initialized`, {
+  version: constants.version,
+  localUrl: `http://localhost:${constants.webuiListenPort}`,
+  baseUrl: constants.webuiBaseUrl
+});
